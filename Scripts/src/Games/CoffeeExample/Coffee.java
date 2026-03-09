c449e7a90ce4e8a5def7e44eb616bc5922c0a1b1
DoctorKrill08 <doctorkrill08@gmail.com>
2026-03-08 20:32:57 -0400

    Added coffee example

---
+++ Scripts/src/Games/CoffeeExample/Coffee.java
@@ -0,0 +1,23 @@
+package Games.CoffeeExample;
+
+public class Coffee {
+    final int MAX_COFFEE = 5;
+    int coffee = MAX_COFFEE;
+    public int getCoffee(){
+        return coffee;
+    }
+    public void drink(){
+        System.out.println("DRINK");
+        if (coffee > 0){
+            coffee = coffee - 1;
+        }else{
+            System.out.println("EMPTY");
+        }
+        System.out.println(coffee + " sips left");
+    }
+    public void refill(){
+        coffee = MAX_COFFEE;
+        System.out.println("REFILL");
+        System.out.println(coffee + " sips left");
+    }
+}
---
+++ Scripts/src/Games/CoffeeExample/Handler.java
@@ -0,0 +1,15 @@
+package Games.CoffeeExample;
+
+public class Handler {
+    public static void main(String[] args) {
+       Coffee coffee = new Coffee();
+       coffee.drink();
+       coffee.drink();
+       coffee.drink();
+       coffee.drink();
+       coffee.drink();
+       coffee.drink();
+       coffee.refill();
+       coffee.drink();
+    }
+}
