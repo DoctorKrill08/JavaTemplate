eb01462be590520c2b8ad11d5bb8fb3fbee722d6
DoctorKrill08 <doctorkrill08@gmail.com>
2026-03-23 22:09:45 -0400

    Fixed not printing

--- Scripts/src/Games/ControlGame/Systems/FeedForward.java
+++ Scripts/src/Games/ControlGame/Systems/FeedForward.java
@@ -6,9 +6,10 @@ public class FeedForward {
     public boolean voltageNormalization = false;
     public double calculate(double target, double voltage){
         if (voltageNormalization) {
+            System.out.println("FF: " + (target * kF * (defaultVoltage / voltage)));
             return (target * kF) * (defaultVoltage / voltage);
         }
-        System.out.println("FF: " + (target * kF * (defaultVoltage / voltage)));
+        System.out.println("FF: " + (target * kF));
         return (target * kF);
     }
 }
